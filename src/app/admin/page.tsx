50d94eee308fcf7a4cc1b676f19a7b15bb33954d
STEPHEN NGARUIYA <123887768+S-t740@users.noreply.github.com>
2026-07-22 16:31:02 +0300

    Add comment in AdminRedirect function
    Added a comment for development purposes.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,3 +4,4 @@ import { redirect } from "next/navigation";
 export default function AdminRedirect() {
     redirect("/dashboard/admin");
 }
+/*did we make any changes here? just for the development*/
